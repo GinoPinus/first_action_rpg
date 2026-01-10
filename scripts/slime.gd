47935a61793c6f67492ee857cc54cfb691ff1928
GinoPinus <csonosimone@gmail.com>
2026-01-10 20:31:57 +0100

    created slime scene and tested view cone

---
+++ scripts/slime.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
+
+func _on_view_cone_body_entered(body: Node2D) -> void:
+	print("I see the player")
+
+
+func _on_view_cone_body_exited(body: Node2D) -> void:
+	print("I don't see the player")
